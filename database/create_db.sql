e4c34c48b8b5430050aa21821351eeccb4799432
Noan Cloarec <noan.cloarec@gmail.com>
2019-12-29 11:03:56 +0700

    zoom & bbox on land masses

--- database/create_db.sql
+++ database/create_db.sql
@@ -2,29 +2,47 @@ CREATE SCHEMA mapisto;
 
 SET search_path TO mapisto;
 
-CREATE TABLE States(
-    state_id serial PRIMARY KEY
+CREATE TABLE states(
+    state_id SERIAL PRIMARY KEY
 );
 
-CREATE TABLE StateNames(
+CREATE TABLE state_names(
     name VARCHAR(255),
     state_id INTEGER REFERENCES states(state_id),
     validity_start TIMESTAMP NOT NULL,
     validity_end TIMESTAMP NOT NULL,
-    color VARCHAR(6)
+    color VARCHAR(7)
 );
 
-CREATE TABLE Territories(
-    territory_id serial PRIMARY KEY,
-    d_path TEXT NOT NULL,
-    precision_in_km REAL NOT NULL,
+CREATE TABLE territories(
+    territory_id SERIAL PRIMARY KEY,
     validity_start TIMESTAMP NOT NULL,
     validity_end TIMESTAMP NOT NULL,
+    min_x REAL NOT NULL,
+    max_x REAL NOT NULL,
+    min_y REAL NOT NULL,
+    max_y REAL NOT NULL,
     state_id INTEGER REFERENCES states(state_id)
 );
 
-CREATE TABLE LandMasses(
-    land_mass_id serial PRIMARY KEY,
+CREATE TABLE territories_shapes(
     d_path TEXT NOT NULL,
-    precision_in_km REAL NOT NULL
+    precision_in_km NUMERIC(6, 1) NOT NULL,
+    territory_id INTEGER REFERENCES territories(territory_id),
+    PRIMARY KEY (precision_in_km, territory_id)
+);
+
+CREATE TABLE lands(
+    land_id SERIAL PRIMARY KEY,
+    min_x REAL NOT NULL,
+    max_x REAL NOT NULL,
+    min_y REAL NOT NULL,
+    max_y REAL NOT NULL
+);
+
+CREATE TABLE lands_shapes(
+    d_path TEXT NOT NULL,
+    precision_in_km NUMERIC(6,1) NOT NULL,
+    land_id INTEGER REFERENCES lands(land_id),
+    PRIMARY KEY (precision_in_km, land_id)
 );
